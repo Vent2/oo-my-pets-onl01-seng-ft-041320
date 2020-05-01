a3b5e5629b39b8895e5b9698ca874964b828333f
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-05-01 15:27:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@all = []
+  def initialize(name, owner_name)
+    @name = name
+    self.owner = owner_name
+    @mood = "happy"
+    @@all << self
+  end
+  def self.all
+    @all
+  end
 end
